9a1f6b2d475443501c2884538e43306fd3524a6b
Lisdanay Hernandez <Lhernandez1848@conestogac.on.ca>
2020-04-10 12:47:04 -0400

    merged CalendarRecommendationInstrumentedTest

---
+++ app/src/androidTest/java/io/github/technocrats/capstone/CalendarRecommendationInstrumentedTest.java
@@ -0,0 +1,26 @@
+package io.github.technocrats.capstone;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.SmallTest;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public class CalendarRecommendationInstrumentedTest {
+    @Rule
+    public ActivityTestRule<CalendarRecommendation> activityTestRule =
+            new ActivityTestRule<>(CalendarRecommendation.class);
+
+    @Test
+    public void clickToSelectSuccessful() {
+        onView(withId(R.id.tvDate)).perform(click());
+    }
+}
